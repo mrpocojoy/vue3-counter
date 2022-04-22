12c0d54408d5b7823f949589610d51cce8ff2afa
mrjow <pocojoy@hotmail.com>
2022-04-22 18:19:03 +0200

    Vue 3 APP

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+
+  export default {
+    data: () => ({
+      contador: 200,
+    }),
+    methods: {
+      incrementar() {   this.contador++;   },
+      disminuir()   {   this.contador--;   },
+      resetear()    {   this.contador = 10;   },
+    },
+    
+    
+  };
+
+
+</script>
+
+
+<template>
+
+  <div class="container">
+    <img src="./assets/logo.svg" alt="" width="125" height="125"/>
+    <h1>Hola Mundo desde VUEJS3</h1>
+    <hr />
+    <h2>Contador: {{ contador }}</h2>
+    <button @click="incrementar()">+1</button>
+    <button @click="disminuir()">-1</button>
+    <button @click="resetear()">Reset</button>
+  </div>
+
+</template>
